25706ffdfcfeb74a7aa8218919a3636de523fd6f
kwxue <55603752+kwxue@users.noreply.github.com>
2020-11-16 19:20:52 -0700

    made progress on repo

---
+++ src/Point.java
@@ -0,0 +1,26 @@
+
+public class Point {
+	private float x;
+	private float y;
+	private int index;
+	
+	public Point(float x, float y, int index) {
+		this.x=x;
+		this.y=y;
+		this.index=index;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+	
+	
+}
--- src/Repository.java
+++ src/Repository.java
@@ -1,6 +1,145 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Observable;
+import java.util.Scanner;
 
-public class Repository {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+public class Repository extends Observable {
+	private static Repository repositoryInstance = null;
+	ArrayList<Point> points;
+	ArrayList<Path> paths;
+	ArrayList<Path> sortedPaths;
+	String status;
+	int threadPointIndex;
+	
+	private Repository() {
+		this.points = new ArrayList<>();
+		this.paths = new ArrayList<>();
+		this.status = "RUN";
+		this.threadPointIndex = 0;
 	}
+	
+	public static Repository getInstance() {
+		if(repositoryInstance==null) {
+			repositoryInstance = new Repository();
+    	}
+    	return repositoryInstance;			
+	}
+	
+	public void readPointsFromFile(String filename) {
+		float xMin,yMin,xMax,yMax;
+		xMin = yMin = Integer.MAX_VALUE;
+		xMax = yMax = Integer.MIN_VALUE;
+		points.clear();//DEBUGGING ONLY
+		File dataFile = new File(filename);
+		try {
+			Scanner fileReader = new Scanner(dataFile);
+			while(fileReader.hasNextLine()) {
+				String[] splitLine = fileReader.nextLine().split(" ");
+				if (isNumeric(splitLine[0])) {
+					float x = Float.parseFloat(splitLine[1]);
+					float y = Float.parseFloat(splitLine[2]);
+					addPoint(x,y);
+					if(x < xMin)
+						xMin = x;
+					if(x > xMax)
+						xMax = x;
+					if(y < yMin)
+						yMin = y;
+					if(y > yMax)
+						yMax = y;
+				}
+			}
+			fileReader.close();
+			normalizePoints(xMin,xMax,yMin,yMax);
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("The file could not be found.");
+		}
+		printPoints(); // DEBUGGING only
+	}
+	
+	public void normalizePoints(float xMin, float xMax, float yMin, float yMax) {
+		int WINDOW_LENGTH = 900;
+		int WINDOW_HEIGHT = 700;
+		
+		for (int i = 0; i < points.size(); i++) {
+			int index = points.get(i).getIndex();
+			float x = points.get(i).getX();
+			float y = points.get(i).getY();
+			x = Math.round(  ((x-xMin)/(xMax - xMin)) * WINDOW_LENGTH);
+			y = WINDOW_HEIGHT - Math.round(  ((y-yMin)/(yMax - yMin)) * WINDOW_HEIGHT);
+			points.set(i, new Point(x,y,index));
+		}
+	}
+	
+	public void savePoints() {
+		//subtract y from window height when inverting y back
+	}
+	
+	private void printPoints() {
+		for (Point point : points) {
+			System.out.println("index: " + point.getIndex() + "\tx: " + point.getX() + "\ty: " +point.getY());
+		}
+	}
+	
+	private boolean isNumeric(String str) {
+		if(str == null || str.length() == 0)
+			return false;
+		return str.chars().allMatch(Character::isDigit);
+	}
+	
+	public void addPath(Path path) {
+		this.paths.add(path);
+	}
+	
+	public void addPoint(float x,float y) {
+		points.add(new Point(x,y,points.size()));
+	}
+	
+	public void notifyView() {
+		setChanged();
+		notifyObservers();
+	}
+	
+	public Point getNextPoint() {
+		return points.get(threadPointIndex++);
+	}
+	
+	public boolean hasNextPoint() {
+		if(threadPointIndex < points.size())
+			return true;
+		return false;
+	}
+
+	public ArrayList<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(ArrayList<Point> points) {
+		this.points = points;
+	}
+
+	public ArrayList<Path> getPaths() {
+		return paths;
+	}
+
+	public ArrayList<Path> getSortedPaths() {
+		return sortedPaths;
+	}
+
+	public void setSortedPaths(ArrayList<Path> sortedPaths) {
+		this.sortedPaths = sortedPaths;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public int getThreadPointIndex() {
+		return threadPointIndex;
+	}
+
+	
 }
